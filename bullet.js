1603d8e24c986b51ba72d517989bece180f6a4d4
Mike D <akagetsu@gmail.com>
2016-04-23 18:30:25 +0100

    Add a bullet class

---
+++ bullet.js
@@ -0,0 +1,18 @@
+function Bullet(game) {
+	this.game = game;
+	this.bullets = null;
+}
+
+Bullet.prototype.init = function init() {
+	this.bullets = this.game.add.group();
+	this.bullets.enableBody = true;
+	this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+
+	this.bullets.createMultiple(50, 'bullet', 0);
+	this.bullets.callAll('animations.add', 'animations', 'bullet', [0, 1, 2, 3], 10, true);
+	this.bullets.callAll('play', null, 'bullet');
+	this.bullets.setAll('checkWorldBounds', true);
+	this.bullets.setAll('outOfBoundsKill', true);
+
+	return this.bullets;
+};
