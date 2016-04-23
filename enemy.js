7112d7ef0b6058971da1633241eab8c12b3fb82c
Mike D <akagetsu@gmail.com>
2016-04-23 20:47:16 +0100

    Add enemy in game

---
+++ enemy.js
@@ -0,0 +1,22 @@
+function Enemy(game) {
+	this.game = game;
+	this.enemyHolder = null;
+}
+
+Enemy.prototype.init = function init() {
+	this.enemyHolder = game.add.group();
+
+	this.enemyHolder.enableBody = true;
+	this.game.time.events.loop(Phaser.Timer.SECOND * 0.7, this.spawn, this);
+};
+
+Enemy.prototype.spawn = function spawn() {
+	var enemy = this.enemyHolder.create(Math.floor(Math.random() * (this.game.world.width - 200)) + 100, 0, 'issue');
+
+	enemy.body.gravity.y = 300;
+	enemy.scale.setTo(1.5);
+};
+
+Enemy.prototype.getEnemies = function getEnemies() {
+	return this.enemyHolder;
+};
