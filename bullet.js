9a31f2329f94bb9e6e77d2553a6cc2936afcb6de
Mike D <akagetsu@gmail.com>
2016-04-23 20:47:02 +0100

    Fix new bullet spritesheet loading

--- bullet.js
+++ bullet.js
@@ -9,7 +9,7 @@ Bullet.prototype.init = function init() {
 	this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
 
 	this.bullets.createMultiple(50, 'bullet', 0);
-	this.bullets.callAll('animations.add', 'animations', 'bullet', [0, 1, 2, 3], 3, true);
+	this.bullets.callAll('animations.add', 'animations', 'bullet', [0, 1], 10, true);
 	this.bullets.callAll('play', null, 'bullet');
 	this.bullets.setAll('checkWorldBounds', true);
 	this.bullets.setAll('outOfBoundsKill', true);
