c91842e4a7f8801a394270f30756e1b8490e356e
Mike D <akagetsu@gmail.com>
2016-11-30 20:36:02 +0000

    Fix keyboard initialisation

--- controls.js
+++ controls.js
@@ -7,15 +7,17 @@ function Controls(game) {
 Controls.prototype.initialiseGamepad = function initialiseGamepad() {
     this.game.input.gamepad.start();
     this.pad = game.input.gamepad.pad1;
+
     return this.pad;
 };
 
 Controls.prototype.initialiseKeyboard = function initialiseKeyboard() {
-    this.keyboard = {};
-    this.keyboard.cursorKeys = this.game.input.keyboard.createCursorKeys();
-    this.keyboard.attackKey = this.game.input.keyboard.addKey(Phaser.Keyboard.X);
-    this.keyboard.jumpKey = this.game.input.keyboard.addKey(Phaser.Keyboard.Z);
-    this.keyboard.restartKey = this.game.input.keyboard.addKey(Phaser.Keyboard.P);
+    this.keyboard = {
+        cursorKeys: this.game.input.keyboard.createCursorKeys(),
+        attackKey: this.game.input.keyboard.addKey(Phaser.Keyboard.X),
+        jumpKey: this.game.input.keyboard.addKey(Phaser.Keyboard.Z),
+        restartKey: this.game.input.keyboard.addKey(Phaser.Keyboard.P)
+    };
 
     return this.keyboard;
 };
